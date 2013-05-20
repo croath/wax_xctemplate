d2a0f10ff08d05db9ef87f1eadd6ddfcf696cd5f
Croath <croathliu@gmail.com>
2013-05-20 17:56:02 +0800

    init.

---
+++ README.md
@@ -0,0 +1,6 @@
+wax_xctemplate
+==============
+
+ - 0.Drag lua folder to ~/Library/Developer/Xcode/Templates/File Templates
+
+ - 1.done
---
+++ lua/Lua class.xctemplate/NSObject/___FILEBASENAME___.lua
@@ -0,0 +1,15 @@
+--
+--  ___FILENAME___
+--  ___PROJECTNAME___
+--
+--  Created by ___FULLUSERNAME___ on ___DATE___.
+--  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+--
+
+waxClass{ "___FILEBASENAMEASIDENTIFIER___", NSObject }
+function init(self)
+  self.super:init()
+  if self then
+  end
+  return self
+end
